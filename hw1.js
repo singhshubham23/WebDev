7d60e8912dc631458fb7b887f59bef0d649000a1
Shubham Kumar <142138865+singhshubham23@users.noreply.github.com>
2024-03-04 20:56:46 +0530

    Add files via upload
    HW 1

---
+++ hw1.js
@@ -0,0 +1,83 @@
+//1. Write a single line comment which says, comments can make code readable
+// Comments can be read by double forward slashes
+
+//2. Write another single comment which says, Welcome to 30DaysOfJavaScript
+//console.log("Welcome to 30 days of JavaScript");
+
+/* 3.Write a multiline comment which says, comments can make code readable, easy to
+reuse and informative */
+//console.log("Comments make code readable, easy to use and informative");
+
+
+//  4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+/*
+let a = 10;
+let s = "String";
+let sum = true;
+let z = undefined;
+let n = null;
+
+console.log(a,s,sum,z,n); */
+
+
+// 5. Create datatypes.js file and use the JavaScript typeof operator to checkdifferent data types. Check the data type of each variable
+
+/*
+let num = 10;
+let str  = "tea";
+let boolean = true;
+let arr =[1,2,3,4];
+
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof boolean);
+console.log(typeof arr);
+*/
+
+// 6. Declare four variables without assigning values
+
+/*
+let var1;
+let var2;
+let var3;
+let var4;
+*/
+
+// 7. Declare four variables with assigned values
+
+/*
+let var1 = 11;
+let var2 = 12;
+let var3 = 13;
+let var4 = 14;
+*/
+
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+/*
+let firstName = "Shubham";
+let lastName = "Singh";
+let maritalStatus = "Single";
+let country = "India";
+let age = "21";
+
+console.log("The name is",firstName,lastName,".", "He is", maritalStatus,"."," Lives in ", country, "and age is", age);
+*/
+
+// 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+
+/*
+let firstName = "Shubham", lastName = "Singh", maritalStatus = "Single", country = "India", age = "21";
+console.log("The name is",firstName,lastName,".", "He is", maritalStatus,"."," Lives in ", country, "and age is", age);
+*/
+
+// 10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+/*
+let myAge = 21;
+let yourAge = 20;
+
+console.log("My age is ",myAge,"and Your age is",yourAge,".");
+*/
